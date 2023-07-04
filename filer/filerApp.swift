66b67c903c52aca55ba4cd38fa84ea6aa5d2945e
nami-minami <boss_cat128@yahoo.co.jp>
2023-07-05 02:59:52 +0900

    Initial Commit

---
+++ filer/filerApp.swift
@@ -0,0 +1,17 @@
+//
+//  filerApp.swift
+//  filer
+//
+//  Created by 南奈弥 on 2023/07/05.
+//
+
+import SwiftUI
+
+@main
+struct filerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

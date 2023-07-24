b9c5b887d129c0b60161c580dc62c6dc6976b84b
nami-minami <boss_cat128@yahoo.co.jp>
2023-07-24 12:11:21 +0900

    first commit

--- filer/ContentView.swift
+++ filer/ContentView.swift
@@ -6,16 +6,70 @@
 //
 
 import SwiftUI
-
+ 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        NavigationView {
+            List {
+                ForEach(getFolder(URL(fileURLWithPath: "/System")), id: \.self) {
+                    url in
+                    ItemCell(url:url)
+                }
+            }
+            .navigationTitle("フォルダ検索")
         }
-        .padding()
+    }
+}
+ 
+/// アイテムセル表示View
+/// URLで指定された１アイテムを表示する
+/// フォルダの場合はサブフォルダへのリンク、その他の場合はファイル名を表示
+struct ItemCell: View {
+    let url: URL
+    var body: some View {
+        HStack {
+            if url.hasDirectoryPath {
+                /// フォルダ
+                NavigationLink(destination: SubFolder(url: url)) {
+                    Image(systemName: "folder")
+                    Text(url.lastPathComponent + "/")
+                }
+            } else {
+                /// ファイル
+                Image(systemName: "doc.text")
+                Text(url.lastPathComponent)
+            }
+        }
+    }
+}
+ 
+/// サブフォルダ表示View
+struct SubFolder: View {
+    let url: URL
+    var body: some View {
+        List {
+            ForEach(getFolder(url), id: \.self) { url in
+                ItemCell(url:url)
+            }
+        }
+    }
+}
+ 
+/// フォルダ取得
+/// - Parameter url: 情報を取得するフォルダのURL
+/// - Returns: 指定されたフォルダに含まれるアイテムのURLリストを返す
+func getFolder(_ url: URL) -> [URL] {
+    do {
+        /// フォルダのURLからふくまれるURLを取得
+        var fileAndFolderURLs = try FileManager.default.contentsOfDirectory(at: url, includingPropertiesForKeys: nil)
+ 
+        /// ファイル名でソート
+        fileAndFolderURLs.sort(by: {$0.lastPathComponent < $1.lastPathComponent})
+        
+        return fileAndFolderURLs
+    } catch {
+        print(error.localizedDescription)
+        return []
     }
 }
 
@@ -24,3 +78,6 @@ struct ContentView_Previews: PreviewProvider {
         ContentView()
     }
 }
+
+
+
